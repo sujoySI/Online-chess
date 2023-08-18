66b1a698d5c28d784d45d116f25a97a8cb0aabc0
Sujoy Sinha <900sujoysinha@gmail.com>
2023-08-18 15:31:45 +0530

    hovering over tile works

--- Assets/Scripts/ChessBoard.cs
+++ Assets/Scripts/ChessBoard.cs
@@ -7,6 +7,9 @@ public class ChessBoard : MonoBehaviour
 {
     [Header("Art Stuff")]
     [SerializeField] private Material tileMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3 boardCentre = Vector3.zero;
     
     //Logic
     private const int TILE_COUNT_X = 8;
@@ -14,11 +17,12 @@ public class ChessBoard : MonoBehaviour
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+    private Vector3 bounds;
 
     
     private void Awake()
     {
-        GenarateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
+        GenarateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
     }
     private void Update()
     {
@@ -30,7 +34,7 @@ public class ChessBoard : MonoBehaviour
 
         RaycastHit info;
         Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile")))
+        if(Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile","Hover")))
         {
             // get the indices of tile that i've hit
             Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
@@ -63,6 +67,9 @@ public class ChessBoard : MonoBehaviour
     //Generate the Board
     private void GenarateAllTiles(float tileSize, int tileCountX, int tileCountY)
     {
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileCountX / 2) * tileSize, 0, (tileCountY / 2) * tileSize) + boardCentre;
+
         tiles = new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
         {
@@ -74,7 +81,7 @@ public class ChessBoard : MonoBehaviour
     }
     private GameObject GenerateSingleTile(float tileSize, int x, int y)
     {
-        GameObject tileObject = new GameObject(string.Format("X:{0},Y:{0}",x,y));
+        GameObject tileObject = new GameObject(string.Format("X:{0},Y:{1}",x,y));
         tileObject.transform.parent = transform;
 
         Mesh mesh = new Mesh();
@@ -82,10 +89,10 @@ public class ChessBoard : MonoBehaviour
         tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
 
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector3(x * tileSize,0,y * tileSize);
-        vertices[1] = new Vector3(x * tileSize,0,(y+1) * tileSize);
-        vertices[2] = new Vector3((x+1) * tileSize,0,y * tileSize);
-        vertices[3] = new Vector3((x + 1) * tileSize,0,(y + 1) * tileSize);
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y+1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x+1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x + 1) * tileSize, yOffset, (y + 1) * tileSize) - bounds;
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
@@ -102,9 +109,9 @@ public class ChessBoard : MonoBehaviour
     //Operations
     private Vector2Int LookupTileIndex(GameObject hitInfo)
     {
-        for (int x=0;x<TILE_COUNT_X;x++)
+        for (int x = 0; x < TILE_COUNT_X; x++)
         {
-            for ( int y=0;y<TILE_COUNT_Y;y++)
+            for ( int y = 0; y < TILE_COUNT_Y; y++)
             {
                 if (tiles[x,y] == hitInfo)
                     return new Vector2Int(x,y);
